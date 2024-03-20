7d8bad70c6f922ead5c146480ee8ee2952fcf164
Evgeny <evgeny.efimenko@proton.me>
2024-03-20 19:38:20 +0500

    feat: create products page & cart page

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { ProductsPage } from "./pages/products"
-import { CartPage } from "./pages/cart"
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ProductsPage/>}/>
-        <Route path="/cart" element={<CartPage/>}/>
+        <Route path="/" />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/products/ui/index.tsx
@@ -0,0 +1,7 @@
+export const ProductsPage = () => {
+  return (
+    <div>
+      Products page
+    </div>
+  );
+};
